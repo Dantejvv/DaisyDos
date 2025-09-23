827a35b291b7b51df6f4a1ec0fdb022773ef8aa4
Dante V <dantejjvercelli@gmail.com>
2025-09-23 09:24:52 -0700

    Initial Commit

---
+++ DaisyDos/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DaisyDos
+//
+//  Created by Dante Vercelli on 9/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DaisyDosTests/DaisyDosTests.swift
@@ -0,0 +1,17 @@
+//
+//  DaisyDosTests.swift
+//  DaisyDosTests
+//
+//  Created by Dante Vercelli on 9/23/25.
+//
+
+import Testing
+@testable import DaisyDos
+
+struct DaisyDosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
